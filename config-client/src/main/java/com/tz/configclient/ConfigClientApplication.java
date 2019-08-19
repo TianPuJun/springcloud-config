6c82b2491cbaa086f79103fd8bea40f493ef8e52
tz <3128379695@qq.com>
2019-08-19 15:44:22 +0800

    消息总线，发送post请求，自动更新配置，无需重启

--- config-client/src/main/java/com/tz/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/tz/configclient/ConfigClientApplication.java
@@ -3,12 +3,25 @@ package com.tz.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 
 @RestController
 @SpringBootApplication
 public class ConfigClientApplication {
+    /**
+     * 刷新配置，无需重启服务，post 请求
+     * /actuator/bus-refresh接口可以指定服务，
+     * 即使用"destination"参数，比如 “/actuator/bus-refresh?destination=customers:**”
+     * 即刷新服务名为customers的所有服务。
+     * http://localhost:8881/actuator/bus-refresh
+     */
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
